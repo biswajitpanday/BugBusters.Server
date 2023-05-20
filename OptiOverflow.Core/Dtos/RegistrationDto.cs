e6313e04317c99253fd82748716f4eae1ae64537
biswajit.panday <biswajit.panday@episerver.com>
2023-05-21 01:02:44 +0600

    Create BaseEntity, ApiResponse, LoginDto, RegistrationDto.

---
+++ OptiOverflow.Core/Dtos/RegistrationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptiOverflow.Core.Dtos;
+
+public class RegistrationDto
+{
+    [Required(ErrorMessage = "UserName is required")]
+    public string? UserName { get; set; }
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required")]
+    public string? Email { get; set; }
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
---
+++ OptiOverflow.Core/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace OptiOverflow.Core.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public bool IsDeleted { get; set; }
+}
