bcc3586ac47d7963c3f5f7c2cae9dca737704882
Biswajit Panday <biswajitmailid@gmail.com>
2023-05-29 04:25:25 +0600

    Fix.

--- OptiOverflow.Core/Interfaces/Services/IQuestionService.cs
+++ OptiOverflow.Core/Interfaces/Services/IQuestionService.cs
@@ -7,6 +7,6 @@ public interface IQuestionService
     Task<List<QuestionDto>> GetAll();
     Task<QuestionDto?> GetById(Guid id);
     Task<QuestionDto> Create(QuestionCreateDto question, string userId);
-    Task<QuestionDto?> Update(QuestionUpdateDto question, Guid id, string? userId);
+    Task<QuestionDto?> Update(QuestionUpdateDto questionUpdateDto, Guid id, string? userId);
     Task Delete(Guid id);
 }
