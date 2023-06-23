0cde569160ea42ca19327e928cf4de64edd2b4c4
biswajitpanday <biswajitmailid@gmail.com>
2023-06-23 21:26:35 +0000

    User Get By Id API.

--- OptiOverflow.Core/Dtos/UserResponseDto.cs
+++ OptiOverflow.Core/Dtos/UserResponseDto.cs
@@ -5,8 +5,8 @@ namespace OptiOverflow.Core.Dtos;
 
 public class UserResponseDto : IMapFrom<UserProfile>
 {
-    public string UserName { get; set; }
-    public string Email { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
     public bool EmailConfirmed { get; set; }
     public bool LockoutEnabled { get; set; }
 
--- OptiOverflow.Service/UserService.cs
+++ OptiOverflow.Service/UserService.cs
@@ -26,20 +26,45 @@ public class UserService : IUserService
 
     public async Task<List<UserResponseDto>?> Get()
     {
-        var user = await _userManager.Users.ToListAsync();
+        var applicationUsers = await _userManager.Users.ToListAsync();
         var userProfile = await _profileRepository.ListAsync();
 
+        if (!applicationUsers.Any() || !userProfile.Any())
+            return null;
+
         var userList = new List<UserResponseDto>();
-        foreach (var applicationUser in user)
+        foreach (var applicationUser in applicationUsers)
         {
             var profile = userProfile.First(x => x.AccountId == applicationUser.Id);
             var userResponseDto = _mapper.Map<UserResponseDto>(profile);
-            userResponseDto.UserName = applicationUser.UserName;
-            userResponseDto.Email = applicationUser.Email;
-            userResponseDto.EmailConfirmed = applicationUser.EmailConfirmed;
-            userResponseDto.LockoutEnabled = applicationUser.LockoutEnabled;
+            MapApplicationUser(userResponseDto, applicationUser);
             userList.Add(userResponseDto);
         }
         return userList;
     }
+
+    public async Task<UserResponseDto?> GetById(Guid id)
+    {
+        var applicationUser = await _userManager.FindByIdAsync(id.ToString());
+        var userProfile = await _profileRepository.Queryable.FirstOrDefaultAsync(x => x.AccountId == applicationUser.Id);
+
+        if(applicationUser == null || userProfile == null) 
+            return null;
+
+        var userResponseDto = _mapper.Map<UserResponseDto>(userProfile);
+        MapApplicationUser(userResponseDto, applicationUser);
+        return userResponseDto;
+    }
+
+    #region Private Methods
+
+    private static void MapApplicationUser(UserResponseDto userResponseDto, ApplicationUser applicationUser)
+    {
+        userResponseDto.UserName = applicationUser.UserName;
+        userResponseDto.Email = applicationUser.Email;
+        userResponseDto.EmailConfirmed = applicationUser.EmailConfirmed;
+        userResponseDto.LockoutEnabled = applicationUser.LockoutEnabled;
+    }
+
+    #endregion
 }
