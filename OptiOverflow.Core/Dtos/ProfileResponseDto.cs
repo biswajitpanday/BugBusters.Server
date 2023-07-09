1597f419b0af194ff4928529ddc918da14ddfa11
biswajitpanday <biswajitmailid@gmail.com>
2023-07-09 19:05:21 +0000

    Fix ProfileResponse.

--- OptiOverflow.Core/Dtos/ProfileResponseDto.cs
+++ OptiOverflow.Core/Dtos/ProfileResponseDto.cs
@@ -8,7 +8,11 @@ public class ProfileResponseDto : IMapFrom<ApplicationUser>
     public string? FirstName { get; set; }
     public string? MiddleName { get; set; }
     public string? LastName { get; set;}
-    public string? Email { get; set; }
+    public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
     public DateTime? DateOfBirth { get; set; }
+
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdated { get; set; }
 }
