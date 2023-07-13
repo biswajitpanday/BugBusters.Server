e00a91b1e69a4a43d56d4f6f35629c8db9606acf
biswajitpanday <biswajitmailid@gmail.com>
2023-07-13 18:44:45 +0600

    Accept Answer API.

--- OptiOverflow.Core/Interfaces/Services/IAnswerService.cs
+++ OptiOverflow.Core/Interfaces/Services/IAnswerService.cs
@@ -4,6 +4,7 @@ namespace OptiOverflow.Core.Interfaces.Services;
 
 public interface IAnswerService
 {
+    Task<AnswerResponseDto?> Accept(Guid id, Guid userId);
     Task<AnswerResponseDto> Create(AnswerCreateDto answer, Guid userId);
     Task<AnswerResponseDto?> Update(AnswerUpdateDto answer, Guid id, Guid userId);
     Task Delete(Guid id);
