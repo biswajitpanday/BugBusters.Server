1036572cd279072dde00d50843528f3a1ca00aff
biswajitpanday <biswajitmailid@gmail.com>
2023-07-19 14:20:22 +0000

    GetFullName refactor in Profile Response.

--- OptiOverflow.Core/Dtos/ProfileResponseDto.cs
+++ OptiOverflow.Core/Dtos/ProfileResponseDto.cs
@@ -8,6 +8,7 @@ public class ProfileResponseDto : IMapFrom<ApplicationUser>
     public string? FirstName { get; set; }
     public string? MiddleName { get; set; }
     public string? LastName { get; set;}
+    public string? FullName => GetFullName();
     public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
     public DateTime? DateOfBirth { get; set; }
@@ -15,4 +16,14 @@ public class ProfileResponseDto : IMapFrom<ApplicationUser>
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastUpdate { get; set; }
+
+    private string? GetFullName()
+    {
+        var fullName = FirstName;
+        if (MiddleName != null)
+            fullName += $" {MiddleName}";
+        if (LastName != null)
+            fullName += $" {LastName}"; ;
+        return fullName;
+    }
 }
