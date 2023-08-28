f4cbb951cd6f3c56d544be43b10d155e47920afc
biswajitpanday <biswajitmailid@gmail.com>
2023-08-28 19:25:55 +0000

    Cherry pick commit '7789d03'

---
+++ OptiOverflow.UnitTest/AuthTest.cs
@@ -0,0 +1,18 @@
+﻿namespace OptiOverflow.UnitTest;
+
+
+[TestFixture]
+public class AuthTest
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void Login_ShouldReturnSuccess()
+    {
+
+    }
+}
