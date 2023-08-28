bacfa51a52f8b061926d5f70cd48067cb3e9efff
biswajitpanday <biswajitmailid@gmail.com>
2023-08-28 19:22:41 +0000

    Cherry pick commit 'd8de46d'

---
+++ OptiOverflow.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OptiOverflow.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
