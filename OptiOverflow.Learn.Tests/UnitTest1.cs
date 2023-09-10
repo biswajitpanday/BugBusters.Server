adc71dd4770f916c1d380223e03b42e3c9937ee7
biswajitpanday <biswajitmailid@gmail.com>
2023-09-10 23:27:38 +0600

    wip.

---
+++ OptiOverflow.Learn.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OptiOverflow.Learn.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
